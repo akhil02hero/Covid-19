28057268cb379e0959057a72a5c7c7034fd87e37
akhil <41247098+akhil02hero@users.noreply.github.com>
2021-05-05 11:20:28 +0530

    It is a website that provide details of covid-19 in india

---
+++ script.js
@@ -0,0 +1,67 @@
+// fetch the data from covid19india api
+
+fetch("https://api.covid19india.org/data.json")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    states(data);
+  })
+  .catch((err) => console.log(err));
+
+// function to display the details of the covid-19 in india statewise
+
+function states(data) {
+  // for (var i=0;i<data.length;i++)
+  console.log(data.statewise);
+  let td = document.querySelector("#table_content");
+  console.log(td);
+  var arr = Object.values(data.statewise);
+  for (var i = 1; i < arr.length - 1; i++) {
+   
+    console.log(arr[i].confirmed);
+    let state_h1 = document.createElement("h1");
+    state_h1.setAttribute("id", "feeds");
+    let active_h1 = document.createElement("h1");
+    active_h1.setAttribute("id", "feeds");
+    let confirmed_h1 = document.createElement("h1");
+    confirmed_h1.setAttribute("id", "feeds");
+    let recovered_h1 = document.createElement("h1");
+    recovered_h1.setAttribute("id", "feeds");
+    let deaths_h1 = document.createElement("h1");
+    deaths_h1.setAttribute("id", "feeds");
+    
+    let state = document.createElement("strong");
+    let active = document.createElement("strong");
+    let deaths = document.createElement("strong");
+    let recovered = document.createElement("strong");
+    let confirmed = document.createElement("strong");
+    state.appendChild(document.createTextNode(arr[i].state));
+    confirmed.appendChild(document.createTextNode(arr[i].confirmed));
+    active.appendChild(document.createTextNode(arr[i].active));
+    recovered.appendChild(document.createTextNode(arr[i].recovered));
+    deaths.appendChild(document.createTextNode(arr[i].deaths));
+    state_h1.appendChild(state);
+    confirmed_h1.appendChild(confirmed);
+    active_h1.appendChild(active);
+    recovered_h1.appendChild(recovered);
+    deaths_h1.appendChild(deaths);
+    td.appendChild(state_h1);
+    td.appendChild(confirmed_h1);
+    td.appendChild(active_h1);
+    td.appendChild(recovered_h1);
+    td.appendChild(deaths_h1);
+
+  }
+}
+
+
+
+
+ // var html = `
+    // <h1 id="feeds"><strong>${arr[i].state}</strong></h1>
+    // <h1 id="feeds"><strong>${arr[i].confirmed}</strong></h1>
+    // <h1 id="feeds"><strong>${arr[i].active}</strong></h1>
+    // <h1 id="feeds"><strong>${arr[i].recovered}</strong></h1>
+    // <h1 id="feeds"><strong>${arr[i].deaths}</strong></h1>
+    // `;
