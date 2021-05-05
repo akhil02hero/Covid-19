e8b997d3f23667f4f17664f8c3fca23241084650
akhil <41247098+akhil02hero@users.noreply.github.com>
2021-05-05 19:26:21 +0530

    Changing multiple js files

---
+++ plot.js
@@ -0,0 +1,19 @@
+fetch('https://api.covid19india.org/data.json').then((response) => {
+                    response.json().then((data) => {
+                        let datalist = data.statewise;
+                        let state = datalist.map(i => i.state);
+                        let active = datalist.map(i => i.active);
+                        console.log(state);
+                        console.log(active);
+                        var data = [{
+                            x: state,
+                            y: active,
+                            type: "bar"
+                        }];
+                        var layout = {
+                            title: "Covid 19"
+                        };
+
+                        Plotly.newPlot("disdis", data, layout);
+                    })
+                })
