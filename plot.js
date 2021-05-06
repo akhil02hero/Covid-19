d1fde8a0dea78102177b5dfe2c7eb16298ef5302
akhil <41247098+akhil02hero@users.noreply.github.com>
2021-05-06 16:35:24 +0530

    Update plot.js

--- plot.js
+++ plot.js
@@ -1,8 +1,8 @@
 fetch('https://api.covid19india.org/data.json').then((response) => {
                     response.json().then((data) => {
                         let datalist = data.statewise;
-                        let state = datalist.map(i => i.state).slice(0);
-                        let active = datalist.map(i => i.deaths).slice(0);
+                        let state = (datalist.map(i => i.state)).slice(1);
+                        let active = (datalist.map(i => i.deaths)).slice(1);
                         console.log(state);
                         console.log(active);
                         var data = [{
