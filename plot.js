28aa7eec1d56b3659159bade1341d6d0d1d2de4b
akhil <41247098+akhil02hero@users.noreply.github.com>
2021-05-06 16:28:11 +0530

    Update plot.js

--- plot.js
+++ plot.js
@@ -1,8 +1,8 @@
 fetch('https://api.covid19india.org/data.json').then((response) => {
                     response.json().then((data) => {
                         let datalist = data.statewise;
-                        let state = datalist.map(i => i.state);
-                        let active = datalist.map(i => i.deaths);
+                        let state = datalist.map(i => i.state).slice(1);
+                        let active = datalist.map(i => i.deaths).slice(1);
                         console.log(state);
                         console.log(active);
                         var data = [{
@@ -11,7 +11,7 @@ fetch('https://api.covid19india.org/data.json').then((response) => {
                             type: "bar"
                         }];
                         var layout = {
-                            title: "Covid 19"
+                            title: "Covid 19 Death Rate"
                         };
 
                         Plotly.newPlot("disdis", data, layout);
